dd12721b37fdfeffec8aff13ed161ccbefd4563a
Hoanqnqu <nhathoangthuychaub1@gmail.com>
2023-09-28 22:14:17 +0700

    finish UI

--- App/src/screens/Register.tsx
+++ App/src/screens/Register.tsx
@@ -1,22 +1,35 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import {
 	Image,
 	KeyboardAvoidingView,
 	Pressable,
 	SafeAreaView,
+	StyleSheet,
 	Text,
 	View,
 } from 'react-native';
+import type { IDropdownRef } from 'react-native-element-dropdown';
+import { Dropdown } from 'react-native-element-dropdown';
 
 import ButtonWithLoader from '../components/ButtonWithLoader';
 import TextInputWithLable from '../components/TextInputWithLabel';
-import { Picker } from '@react-native-picker/picker';
 // import { showError } from '../utils/helperFunction';
 import { useNavigation } from '@react-navigation/native';
+
+const data = [
+	{ label: 'Item 1', value: '1' },
+	{ label: 'Item 2', value: '2' },
+	{ label: 'Item 3', value: '3' },
+	{ label: 'Item 4', value: '4' },
+	{ label: 'Item 5', value: '5' },
+	{ label: 'Item 6', value: '6' },
+	{ label: 'Item 7', value: '7' },
+	{ label: 'Item 8', value: '8' },
+];
 const Register = () => {
 	const navigation = useNavigation();
-	const [selectedLanguage, setSelectedLanguage] = useState();
-
+	const [value, setValue] = React.useState('key1');
+	const ref = useRef<IDropdownRef>(null);
 	return (
 		<SafeAreaView>
 			<KeyboardAvoidingView
@@ -27,7 +40,7 @@ const Register = () => {
 			>
 				<Image
 					source={require('../assets/images/rentallyLogo.png')}
-					style={{ width: 60, height: 60, marginTop: 20 }}
+					style={{ width: 60, height: 60, marginTop: 30 }}
 				/>
 				<View>
 					<SafeAreaView
@@ -147,15 +160,44 @@ const Register = () => {
 								Which describes best your role?
 							</Text>
 						</View>
-						<Picker
-							selectedValue={selectedLanguage}
-							onValueChange={(itemValue, itemIndex) =>
-								setSelectedLanguage(itemValue)
-							}
+						<View
+							style={{
+								marginVertical: 10,
+								flexDirection: 'row',
+								alignItems: 'center',
+								marginBottom: 15,
+							}}
 						>
-							<Picker.Item label="Java" value="java" />
-							<Picker.Item label="JavaScript" value="js" />
-						</Picker>
+							<Dropdown
+								ref={ref}
+								style={{
+									flex: 1,
+									height: 38,
+									borderWidth: 2,
+									borderRadius: 8,
+									borderColor: '#E1E6EF',
+									backgroundColor: '#F1F3F9',
+								}}
+								placeholderStyle={styles.placeholderStyle}
+								selectedTextStyle={styles.selectedTextStyle}
+								inputSearchStyle={styles.inputSearchStyle}
+								iconStyle={styles.iconStyle}
+								data={data}
+								search
+								maxHeight={300}
+								labelField="label"
+								valueField="value"
+								placeholder="Dropdown 2"
+								searchPlaceholder="Search..."
+								value={value}
+								onChange={(item) => {
+									setValue(item.value);
+								}}
+								// eslint-disable-next-line @typescript-eslint/no-empty-function
+								onChangeText={() => {}} // Keep search keyword
+							/>
+						</View>
+
 						<ButtonWithLoader
 							text="Register"
 							onPress={() => {}}
@@ -169,3 +211,22 @@ const Register = () => {
 };
 
 export default Register;
+const styles = StyleSheet.create({
+	placeholderStyle: {
+		fontSize: 14,
+		color: 'rgba(29, 36, 51, 0.8)',
+	},
+	selectedTextStyle: {
+		fontSize: 14,
+		margin: 8,
+		color: 'rgba(29, 36, 51, 0.8)',
+	},
+	iconStyle: {
+		width: 10,
+		height: 10,
+	},
+	inputSearchStyle: {
+		height: 38,
+		fontSize: 14,
+	},
+});
