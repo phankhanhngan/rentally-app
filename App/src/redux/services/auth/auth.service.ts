2e2967bd267cae89d0578316becbc7fd2c7721c3
Hoanqnqu <nhathoangthuychaub1@gmail.com>
2023-09-29 10:33:27 +0700

    update auth service

--- App/src/redux/services/auth/auth.service.ts
+++ App/src/redux/services/auth/auth.service.ts
@@ -13,18 +13,68 @@ export const authApi = creatApiAuthWithAuth.injectEndpoints({
 		}),
 		register: builder.mutation({
 			query: (body: {
-				name: string;
+				firstName: string;
 				email: string;
 				password: string;
+				lastName: string;
+				phoneNumber: string;
+				role: string;
 			}) => {
 				return {
-					url: '/user/signup',
+					url: '/auth/register',
 					method: 'post',
 					body,
 				};
 			},
 		}),
+		registerVerification: builder.mutation({
+			query: (body: { email: string; code: string }) => ({
+				url: '/auth/verify',
+				method: 'POST',
+				body,
+			}),
+		}),
+		resendEmail: builder.mutation({
+			query: (body: { email: string }) => ({
+				url: '/auth/resend-verification',
+				method: 'POST',
+				body,
+			}),
+		}),
+		forgotPassword: builder.mutation({
+			query: (body: { email: string }) => ({
+				url: '/auth/forgot-password',
+				method: 'POST',
+				body,
+			}),
+		}),
+		forgotPasswordVerify: builder.mutation({
+			query: (body: { email: string; code: string }) => ({
+				url: '/auth/forgot-password/verify',
+				method: 'POST',
+				body,
+			}),
+		}),
+		resetPassword: builder.mutation({
+			query: (body: {
+				email: string;
+				password: string;
+				code: string;
+			}) => ({
+				url: '/auth/reset-password',
+				method: 'POST',
+				body,
+			}),
+		}),
 	}),
 });
 
-export const { useLoginMutation, useRegisterMutation } = authApi;
+export const {
+	useLoginMutation,
+	useRegisterMutation,
+	useRegisterVerificationMutation,
+	useForgotPasswordMutation,
+	useForgotPasswordVerifyMutation,
+	useResendEmailMutation,
+	useResetPasswordMutation,
+} = authApi;
